243dd8bbbbda5cc6566c977e1d1c2c197e65a5fd
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-06-18 13:53:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB5_2oLtuY6qWkB-X1izT_ofIL1dcoOC7U",
-  authDomain: "bibel-19fb0.firebaseapp.com",
-  projectId: "bibel-19fb0",
-  storageBucket: "bibel-19fb0.appspot.com",
-  messagingSenderId: "116871497083",
-  appId: "1:116871497083:web:d24cdc180fa708202fa9de",
-  measurementId: "G-M5E03X0VSR"
-};
+?
 
 
 firebase.initializeApp(firebaseConfig);
